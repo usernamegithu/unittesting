8c4c4066e209405e3b21218116dd83f5127e39ea
usernamegithu <48979444+usernamegithu@users.noreply.github.com>
2019-03-28 15:30:51 +0530

    Update calci.c

--- calci.c
+++ calci.c
@@ -12,7 +12,14 @@ int addition(int a, int b)
 }
  
 
-/*double powe(int a,int b)
+int mod(int a,int b)
 {
- return (pow( a,b));
- }*/
+ if(a>=0&&b>=0)
+ {
+  return a%b;
+ }
+ else
+ {
+  return 0;
+ }
+ }
